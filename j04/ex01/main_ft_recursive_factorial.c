68ffb88ca35eb2c92611347f40ca97c038380741
Antoine DOUSSAUD <antoine@doussaud.org>
2014-07-08 13:17:25 +0200

    ex00ex01

--- j04/ex01/main_ft_recursive_factorial.c
+++ j04/ex01/main_ft_recursive_factorial.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_ft_recursive_factorial.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adoussau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/07/08 12:19:49 by adoussau          #+#    #+#             */
+/*   Updated: 2014/07/08 12:19:58 by adoussau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
 int	ft_recursive_factorial(int nb);
@@ -6,7 +18,7 @@ int	main()
 {
 	int i;
 
-	for(i = 0; i < 12;i++)
+	for(i = -1; i < 14;i++)
 		printf("%d! = %d\n", i, ft_recursive_factorial(i));
 	return 0;
 }
