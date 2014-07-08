f13c1c878af975b3efcdb9719e8eefad59d7f08e
Antoine DOUSSAUD <antoine@doussaud.org>
2014-07-08 11:15:42 +0200

    add main

---
+++ j03/ex07/main_ft_strrev.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/07/06 18:36:02 by aalliot           #+#    #+#             */
+/*   Updated: 2014/07/08 11:08:24 by adoussau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+char	*ft_strrev(char *str);
+
+int		main(void)
+{
+	char c[] = "abcdefg";
+	
+	printf("%s\n", c);			
+	ft_strrev(c);
+	printf("%s\n", c);
+	printf("%s",ft_strrev(c));
+	return (0);
+}
