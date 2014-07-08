2f71b8aeb5fb34d81e7cc93af2b58a2b5ec9b242
Antoine DOUSSAUD <antoine@doussaud.org>
2014-07-08 21:01:54 +0200

    ex05

--- j04/ex05/main_ft_sqrt.c
+++ j04/ex05/main_ft_sqrt.c
@@ -1,27 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_ft_recursive_power.c                          :+:      :+:    :+:   */
+/*   main_ft_sqrt.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adoussau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/07/08 17:25:56 by adoussau          #+#    #+#             */
-/*   Updated: 2014/07/08 18:16:02 by adoussau         ###   ########.fr       */
+/*   Created: 2014/07/08 20:16:42 by adoussau          #+#    #+#             */
+/*   Updated: 2014/07/08 20:33:18 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stdio.h>
 
-int		ft_recursive_power(int nb, int pow);
+int		ft_sqrt(int i);
 
 int		main()
 {
-	int nb;
-	int pow;
-	
-	for ( nb=-2; nb < 10; nb++)
-		for(pow = -2; pow < 10; pow++)
-			printf("%d^%d = %d \n", nb, pow, ft_recursive_power(nb,pow));
+	int i;
+
+	for (i = 1; i < 37 ; i++)
+		printf("sqrt(%d) = %d\n",i ,ft_sqrt(i));
 	return (0);
 }
+
