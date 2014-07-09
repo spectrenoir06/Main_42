59154adbdeb89caeda0dbd84998cff927541e4cc
Antoine DOUSSAUD <antoine@doussaud.org>
2014-07-09 17:52:50 +0200

    ex03j05

---
+++ j05/ex03/main_strcmp.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_strcmp.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adoussau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/07/09 17:23:18 by adoussau          #+#    #+#             */
+/*   Updated: 2014/07/09 17:52:06 by adoussau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		strcmp(char *str1, char *str2);
+
+int		main()
+{
+	char str1[] = "salut je suis le 1er";
+	char str2[] = "salut je suis le 2eme";
+
+	printf("str1 = %s\nstr2 = %s\nft_strcmp(str1, str2)=%d\n", str1, str2, strcmp(str1, str2));
+	return (0);
+}
