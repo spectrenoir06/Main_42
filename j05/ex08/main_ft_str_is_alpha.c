284cd49231a2db467fcc840d190b9f6e96a2e6e6
Anton ALLIOT <anton.alliot@live.se>
2014-07-10 00:36:51 +0200

    main_ft_str_is_alpha.c

--- j05/ex08/main_ft_str_is_alpha.c
+++ j05/ex08/main_ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: adoussau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/07/09 23:19:19 by adoussau          #+#    #+#             */
-/*   Updated: 2014/07/10 00:31:12 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/07/10 00:35:49 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ int		main()
 	char str1[] = "Pate";
 	char str2[] = "42";
 	char str3[] = "Pate en croute de 42";
+	char str4[] = "";
 
 	printf("ft_str_is_alpha(\"%s\") = %d\n",str1,ft_str_is_alpha(str1));
 	printf("ft_str_is_alpha(\"%s\") = %d\n",str2,ft_str_is_alpha(str2));
 	printf("ft_str_is_alpha(\"%s\") = %d\n",str3,ft_str_is_alpha(str3));
+	printf("ft_str_is_alpha(\"%s\") = %d\n",str4,ft_str_is_alpha(str4));
 	return (0);
 }
