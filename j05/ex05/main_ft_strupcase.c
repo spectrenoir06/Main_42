1f23b951e589777949f9674f31dbc8e669bc9ed3
Anton ALLIOT <anton.alliot@live.se>
2014-07-09 21:28:11 +0200

    main_ft_strupcase.c

--- j05/ex05/main_ft_strupcase.c
+++ j05/ex05/main_ft_strupcase.c
@@ -6,7 +6,7 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/07/09 20:58:23 by aalliot           #+#    #+#             */
-/*   Updated: 2014/07/09 21:15:51 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/07/09 21:25:41 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ char	*ft_strupcase(char *str);
 int		main()
 {
 	char	c[] = "Le pate c\'est trop bon!";
-	printf("%s\n%s\n", c, ft_strupcase(c));
+	printf("%s\n", c);
+	printf("%s\n", ft_strupcase(c));
   	return (0);	
 }
