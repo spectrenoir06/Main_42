d88eac6e91a961c17963f1806ca6beaef73e7352
Anton ALLIOT <anton.alliot@live.se>
2014-07-10 01:49:50 +0200

    main_ft_str_is_lowercase.c

---
+++ j05/ex10/main_ft_str_is_lowercase.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_fr_str_is_lowercase.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/07/10 01:39:27 by aalliot           #+#    #+#             */
+/*   Updated: 2014/07/10 01:39:38 by aalliot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int		ft_str_is_lowercase(char *s);
+
+int		main()
+{
+	char str1[] = "pate";
+	char str2[] = "42";
+	char str3[] = "Pate en croute de 42";
+	char str4[] = "";
+
+	printf("ft_str_is_alpha(\"%s\") = %d\n",str1,ft_str_is_lowercase(str1));
+	printf("ft_str_is_alpha(\"%s\") = %d\n",str2,ft_str_is_lowercase(str2));
+	printf("ft_str_is_alpha(\"%s\") = %d\n",str3,ft_str_is_lowercase(str3));
+	printf("ft_str_is_alpha(\"%s\") = %d\n",str4,ft_str_is_lowercase(str4));
+	return (0);
+}
