b6e9998c8627bbaa38efbf19a3edec4b3fcc6b27
Vallium <anton.alliot@live.se>
2014-07-10 20:34:45 +0200

    Update main_strcmp.c

--- j05/ex03/main_strcmp.c
+++ j05/ex03/main_strcmp.c
@@ -19,8 +19,8 @@ int		main()
 {
 	char str1[] = "salut je suis le 1er";
 	char str2[] = "Salut je suis le 2eme";
-	char str3[] = "salut je suis dhawi;a4582a4le 1er";
-	char str4[] = "salut je suahahuiod486413216ais le 2eme";
+	char str3[] = "salut je suas dhawi;a4582a4le 1er";
+	char str4[] = "salut je suihahuiod486413216ais le 2eme";
 
 	printf("str1 = %s\nstr2 = %s\nft_strcmp(str1, str2)=%d\nstrcmp()=%d\n", str1, str2, ft_strcmp(str1, str2),strcmp(str1,str2));
 	printf("str1 = %s\nstr2 = %s\nft_strcmp(str1, str2)=%d\nstrcmp()=%d\n", str3, str4, ft_strcmp(str3, str4),strcmp(str3,str4));
