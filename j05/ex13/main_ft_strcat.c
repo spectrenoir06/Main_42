a4b07182ed8637215dd457e3b89ae952b53974c0
Antoine DOUSSAUD <antoine@doussaud.org>
2014-07-10 10:51:28 +0200

    ex13j05

---
+++ j05/ex13/main_ft_strcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   maim_ft_strcat.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adoussau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/07/10 10:35:08 by adoussau          #+#    #+#             */
+/*   Updated: 2014/07/10 10:48:22 by adoussau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+char *ft_strcat(char *str1, char *str2);
+
+int		main()
+{
+	char str1[200] = "Le Moyen Age fait du pate un chef d'oeuvre,";
+	char str2[] = "\nce qui n\'est au XI eme siecle";
+	char str3[] = "\nqu\'un simple hache de viandes epicees.";
+
+	printf("str1 = %s\n", str1);
+
+	ft_strcat(str1, str2);
+	ft_strcat(str1, str3);
+
+	printf("str2 = %s\n", str2);
+	printf("str3 = %s\n", str3);
+	printf("str1 + str2 + str3 = %s\n", str1);
+	return (0);
+}
+
